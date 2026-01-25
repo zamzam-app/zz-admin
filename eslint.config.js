698e205257dd3cc43a8456142fb446d9b12eeede
zia2010 <1999ziaulla@gmail.com>
2026-01-25 14:50:08 +0530

    feature: initial setup

--- eslint.config.js
+++ eslint.config.js
@@ -3,24 +3,26 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
-import prettier from 'eslint-config-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default defineConfig([
+  globalIgnores(['dist', 'node_modules', '.temp', '.vite']),
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, prettier],
     files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    ignores: ['dist/**', 'node_modules/**', '.temp/**'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    },
   },
-);
+  {
+    files: ['**/*.test.{ts,tsx}'],
+    ignores: ['**/*.test.{ts,tsx}'], // completely skip these files
+  },
+]);
