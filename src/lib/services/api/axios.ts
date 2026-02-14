ec5cef13543fee2895267ac1aaaa40f31dc83925
zia2010 <1999ziaulla@gmail.com>
2026-02-14 13:24:26 +0530

    fix: removed refresh token

--- src/lib/services/api/axios.ts
+++ src/lib/services/api/axios.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { AUTH } from './endpoints';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -27,29 +26,13 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
   (res) => res,
   async (error) => {
-    const originalRequest = error.config;
 
     const tokenString = localStorage.getItem('token');
 
     if (!tokenString || error?.status === 401 || error?.response?.status === 401) {
-      originalRequest._retry = true;
-      try {
-        const response = await api.post(AUTH.REFRESH);
-        const newToken = response.data?.access_token;
-        if (newToken) {
-          // Update localStorage
-          localStorage.setItem('token', JSON.stringify({ token: newToken }));
-
-          // Update retry request header
-          originalRequest.headers.set('Authorization', `Bearer ${newToken}`);
-
-          return api(originalRequest);
-        }
-      } catch {
-        console.log('refresh token failed');
-        localStorage.clear();
-        window.location.href = '/login';
-      }
+      console.log('token failed');
+      localStorage.clear();
+      window.location.href = '/login';
     }
 
     // return Promise.reject(error);
