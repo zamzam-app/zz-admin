d9ec91aafeb8eb1d95d59307ab07a193dd244a91
Abin Anto <abin98anto@gmail.com>
2026-04-06 13:13:45 +0530

    feature: uploaded cakes completed

--- src/lib/types/product.ts
+++ src/lib/types/product.ts
@@ -62,3 +62,32 @@ export interface CustomCakesResponse {
     limit: number;
   };
 }
+
+export interface UploadedCake {
+  _id: string;
+  userId: {
+    _id: string;
+    name?: string;
+    phoneNumber?: string;
+  };
+  name?: string;
+  phone?: string;
+  referenceImageUrl: string;
+  description: string;
+  isActive: boolean;
+  isDeleted: boolean;
+  createdAt: string;
+  updatedAt: string;
+  __v?: number;
+}
+
+export interface UploadedCakesResponse {
+  data: UploadedCake[];
+  meta: {
+    total: number;
+    currentPage: number;
+    hasPrevPage: boolean;
+    hasNextPage: boolean;
+    limit: number;
+  };
+}
